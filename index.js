f2a6c011cf2e44629348c2308787b1a2e3626ad0
HMRussell0 <89594546+HMRussell0@users.noreply.github.com>
2021-09-02 23:16:31 +0600

    add dark file

---
+++ index.js
@@ -0,0 +1,10 @@
+// let toggle = document.querySelector(".toggle");
+// let mainSection  = document.querySelector(".main-section")
+
+// let Body = document.getElementById("mainbody")
+// toggle.addEventListener("click", ()=>{
+//           toggle.classList.toggle("active");
+//           Body.classList.toggle("active");
+//           mainSection.classList.toggle("active")
+          
+// })
---
+++ j.js
@@ -0,0 +1,16 @@
+// // must be an array, could have only one element
+// let visibilityIds = ['#counters_1', '#counters_2', '#counters_3']; 
+
+// // default counter class
+// let counterClass = '.counter';
+
+// // default animation speed
+// let defaultSpeed = 3000; 
+
+
+
+$(document).ready(function(){
+       $('.btn').click(()=>{
+                 $('h1').load('demo.html')
+       })
+})
